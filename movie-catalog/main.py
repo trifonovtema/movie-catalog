693eb0d4a96b8b9c8d8c249db957a0ee70ac55d2
trifonovtema <artyom85@gmail.com>
2025-04-28 08:59:35 +0300

    add root endpoint add docs url to the root endpoint response

--- movie-catalog/main.py
+++ movie-catalog/main.py
@@ -1,5 +1,18 @@
-from fastapi import FastAPI
+import uvicorn
+from fastapi import FastAPI, Request
 
 app = FastAPI(
     title="Movie Catalog",
 )
+
+
+@app.get("/")
+async def root(
+    request: Request,
+):
+    print(request)
+    docs_url = request.url.replace(path="/docs")
+    return {
+        "message": "This is the root of the movie catalog API",
+        "docs": str(docs_url),
+    }
