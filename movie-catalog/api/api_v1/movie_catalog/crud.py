e30a03ff7d0863791d69e597889a28eb802dcc10
trifonovtema <artyom85@gmail.com>
2025-04-28 14:08:21 +0300

    move MOVIES list from main to crud module

---
+++ movie-catalog/api/api_v1/movie_catalog/crud.py
@@ -0,0 +1,28 @@
+from schemas.movie import Movie
+
+MOVIES = [
+    Movie(
+        id=1,
+        name="One",
+        description="Movie One",
+        year=2021,
+    ),
+    Movie(
+        id=2,
+        name="Two",
+        description="Movie Two",
+        year=2022,
+    ),
+    Movie(
+        id=3,
+        name="Three",
+        description="Movie Three",
+        year=2023,
+    ),
+    Movie(
+        id=4,
+        name="Four",
+        description="Movie Four",
+        year=2024,
+    ),
+]
