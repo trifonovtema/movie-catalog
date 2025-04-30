b171364a991cb6a00aad27d0099fc2a61d6ec135
trifonovtema <artyom85@gmail.com>
2025-05-01 00:00:12 +0300

    add LOG_LEVEL and LOG_FORMAT

--- movie-catalog/core/config.py
+++ movie-catalog/core/config.py
@@ -1,4 +1,10 @@
+import logging
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 MOVIE_STORAGE_FILEPATH = BASE_DIR / "movie_storage.json"
+
+LOG_LEVEL = logging.INFO
+LOG_FORMAT: str = (
+    "[%(asctime)s.%(msecs)03d] %(module)10s:%(lineno)-3d %(levelname)-7s - %(message)s"
+)
