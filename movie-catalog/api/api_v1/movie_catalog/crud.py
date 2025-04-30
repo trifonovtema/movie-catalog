2d2519c48484eb38b6a79bac701215a7edd144dd
trifonovtema <artyom85@gmail.com>
2025-04-30 14:08:48 +0300

    add delete movie from storage

--- movie-catalog/api/api_v1/movie_catalog/crud.py
+++ movie-catalog/api/api_v1/movie_catalog/crud.py
@@ -40,6 +40,12 @@ class MovieStorage:
     def create(self, movie: Movie):
         self.slug_to_movie[movie.slug] = movie
 
+    def delete_by_slug(self, slug: str) -> None:
+        self.slug_to_movie.pop(slug, None)
+
+    def delete(self, movie: Movie) -> None:
+        self.delete_by_slug(slug=movie.slug)
+
 
 storage = MovieStorage()
 
