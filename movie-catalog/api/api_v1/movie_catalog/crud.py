40c4807b60eadde9ddf2ea716bb9e572ff024f41
trifonovtema <artyom85@gmail.com>
2025-04-30 23:23:30 +0300

    add save/load to/from disk functionality to storage

--- movie-catalog/api/api_v1/movie_catalog/crud.py
+++ movie-catalog/api/api_v1/movie_catalog/crud.py
@@ -38,6 +38,32 @@ MOVIES = [
 
 class MovieStorage:
     slug_to_movie: dict[str, Movie] = {}
+    file_name: str = "movies_storage.json"
+
+    def __init__(self):
+        self.load_from_disk()
+
+    def save_to_disk(self):
+        with open(self.file_name, "w") as f:
+            for movie in self.slug_to_movie.values():
+                f.write(movie.model_dump_json())
+                f.write("\n")
+
+    def load_from_disk(self):
+        try:
+            with open(self.file_name, "r") as f:
+                for line in f:
+                    try:
+                        movie = Movie.model_validate_json(line)
+                        self.slug_to_movie[movie.slug] = movie
+                    except ValueError as e:
+                        print(f"Invalid movie: {line}. Error: {e}")
+        except FileNotFoundError:
+            self.create_storage()
+
+    def create_storage(self):
+        with open(self.file_name, "w") as f:
+            f.write("")
 
     def get(self):
         return self.slug_to_movie.values()
@@ -47,63 +73,68 @@ class MovieStorage:
 
     def create(self, movie: Movie):
         self.slug_to_movie[movie.slug] = movie
+        self.save_to_disk()
 
     def delete_by_slug(self, slug: str) -> None:
         self.slug_to_movie.pop(slug, None)
+        self.save_to_disk()
 
     def delete(self, movie: Movie) -> None:
         self.delete_by_slug(slug=movie.slug)
+        self.save_to_disk()
 
     def update(self, movie: Movie, movie_in: MovieUpdate) -> Movie:
         for field, value in movie_in:
             setattr(movie, field, value)
+        self.save_to_disk()
         return movie
 
     def update_partial(self, movie: Movie, movie_in: MovieUpdatePartial) -> Movie:
         for field, value in movie_in.model_dump(exclude_unset=True).items():
             setattr(movie, field, value)
+        self.save_to_disk()
         return movie
 
 
 storage = MovieStorage()
 
 
-storage.create(
-    Movie(
-        slug="one",
-        name="One",
-        description="Movie One",
-        year=2021,
-        notes="",
-    )
-)
-
-storage.create(
-    Movie(
-        slug="two",
-        name="Two",
-        description="Movie Two",
-        year=2022,
-        notes="",
-    )
-)
-
-storage.create(
-    Movie(
-        slug="three",
-        name="Three",
-        description="Movie Three",
-        year=2023,
-        notes="",
-    )
-)
-
-storage.create(
-    Movie(
-        slug="four",
-        name="Four",
-        description="Movie Four",
-        year=2024,
-        notes="",
-    )
-)
+# storage.create(
+#     Movie(
+#         slug="one",
+#         name="One",
+#         description="Movie One",
+#         year=2021,
+#         notes="",
+#     )
+# )
+#
+# storage.create(
+#     Movie(
+#         slug="two",
+#         name="Two",
+#         description="Movie Two",
+#         year=2022,
+#         notes="",
+#     )
+# )
+#
+# storage.create(
+#     Movie(
+#         slug="three",
+#         name="Three",
+#         description="Movie Three",
+#         year=2023,
+#         notes="",
+#     )
+# )
+#
+# storage.create(
+#     Movie(
+#         slug="four",
+#         name="Four",
+#         description="Movie Four",
+#         year=2024,
+#         notes="",
+#     )
+# )
