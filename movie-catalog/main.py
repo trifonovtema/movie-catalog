91aac660fce615cae9b981a6916c3ad6e09efd8c
trifonovtema <artyom85@gmail.com>
2025-05-01 00:00:24 +0300

    add log basic config

--- movie-catalog/main.py
+++ movie-catalog/main.py
@@ -1,9 +1,17 @@
+import logging
+
 from fastapi import (
     FastAPI,
     Request,
 )
 
 from api import router as api_router
+from core import config
+
+logging.basicConfig(
+    level=config.LOG_LEVEL,
+    format=config.LOG_FORMAT,
+)
 
 app = FastAPI(
     title="Movie Catalog",
