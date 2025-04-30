63f3608660643699ebd8c32df534b0c0ae01e3d9
trifonovtema <artyom85@gmail.com>
2025-04-30 23:44:36 +0300

    add save_state method add from_state method

--- movie-catalog/api/api_v1/movie_catalog/crud.py
+++ movie-catalog/api/api_v1/movie_catalog/crud.py
@@ -1,3 +1,6 @@
+from pydantic import BaseModel, ValidationError
+
+from core.config import MOVIE_STORAGE_FILEPATH
 from schemas.movie import (
     Movie,
     MovieUpdate,
@@ -36,34 +39,40 @@ MOVIES = [
 ]
 
 
-class MovieStorage:
+class MovieStorage(BaseModel):
     slug_to_movie: dict[str, Movie] = {}
-    file_name: str = "movies_storage.json"
+    # file_name: str = MOVIE_STORAGE_FILEPATH
 
-    def __init__(self):
-        self.load_from_disk()
+    @classmethod
+    def from_state(cls) -> "MovieStorage":
+        if not MOVIE_STORAGE_FILEPATH.exists():
+            return MovieStorage()
+        return cls.model_validate_json(MOVIE_STORAGE_FILEPATH.read_text())
 
-    def save_to_disk(self):
-        with open(self.file_name, "w") as f:
-            for movie in self.slug_to_movie.values():
-                f.write(movie.model_dump_json())
-                f.write("\n")
+    def save_state(self):
+        MOVIE_STORAGE_FILEPATH.write_text(self.model_dump_json(indent=2))
 
-    def load_from_disk(self):
-        try:
-            with open(self.file_name, "r") as f:
-                for line in f:
-                    try:
-                        movie = Movie.model_validate_json(line)
-                        self.slug_to_movie[movie.slug] = movie
-                    except ValueError as e:
-                        print(f"Invalid movie: {line}. Error: {e}")
-        except FileNotFoundError:
-            self.create_storage()
+    # def save_to_disk(self):
+    #     with open(self.file_name, "w") as f:
+    #         for movie in self.slug_to_movie.values():
+    #             f.write(movie.model_dump_json())
+    #             f.write("\n")
+    #
+    # def load_from_disk(self):
+    #     try:
+    #         with open(self.file_name, "r") as f:
+    #             for line in f:
+    #                 try:
+    #                     movie = Movie.model_validate_json(line)
+    #                     self.slug_to_movie[movie.slug] = movie
+    #                 except ValueError as e:
+    #                     print(f"Invalid movie: {line}. Error: {e}")
+    #     except FileNotFoundError:
+    #         self.create_storage()
 
-    def create_storage(self):
-        with open(self.file_name, "w") as f:
-            f.write("")
+    # def create_storage(self):
+    #     with open(self.file_name, "w") as f:
+    #         f.write("")
 
     def get(self):
         return self.slug_to_movie.values()
@@ -73,31 +82,40 @@ class MovieStorage:
 
     def create(self, movie: Movie):
         self.slug_to_movie[movie.slug] = movie
-        self.save_to_disk()
+        # self.save_to_disk()
+        self.save_state()
 
     def delete_by_slug(self, slug: str) -> None:
         self.slug_to_movie.pop(slug, None)
-        self.save_to_disk()
+        # self.save_to_disk()
+        self.save_state()
 
     def delete(self, movie: Movie) -> None:
         self.delete_by_slug(slug=movie.slug)
-        self.save_to_disk()
+        # self.save_to_disk()
+        self.save_state()
 
     def update(self, movie: Movie, movie_in: MovieUpdate) -> Movie:
         for field, value in movie_in:
             setattr(movie, field, value)
-        self.save_to_disk()
+        # self.save_to_disk()
+        self.save_state()
         return movie
 
     def update_partial(self, movie: Movie, movie_in: MovieUpdatePartial) -> Movie:
         for field, value in movie_in.model_dump(exclude_unset=True).items():
             setattr(movie, field, value)
-        self.save_to_disk()
+        # self.save_to_disk()
+        self.save_state()
         return movie
 
 
-storage = MovieStorage()
-
+try:
+    storage = MovieStorage.from_state()
+except ValidationError as e:
+    print(f"Error loading storage: {e}")
+    storage = MovieStorage()
+    storage.save_state()
 
 # storage.create(
 #     Movie(
