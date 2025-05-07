0e32ab7049f4e95bb1db83f02cc10c53f0b5fee0
trifonovtema <artyom85@gmail.com>
2025-05-07 10:51:21 +0300

    add background tasks with save_state only for unsafe methods

--- movie-catalog/api/api_v1/movie_catalog/dependencies.py
+++ movie-catalog/api/api_v1/movie_catalog/dependencies.py
@@ -1,12 +1,25 @@
 import logging
 
-from fastapi import HTTPException, BackgroundTasks
+from fastapi import (
+    HTTPException,
+    BackgroundTasks,
+    Request,
+)
 from starlette import status
 
 from api.api_v1.movie_catalog.crud import storage
 from schemas.movie import Movie
 
 logger = logging.getLogger(__name__)
+
+UNSAFE_METHODS = frozenset(
+    {
+        "POST",
+        "DELETE",
+        "PUT",
+        "PATCH",
+    },
+)
 
 
 def prefetch_movie_by_slug(
@@ -22,8 +35,10 @@ def prefetch_movie_by_slug(
 
 
 def save_movie_storage_state(
+    request: Request,
     background_tasks: BackgroundTasks,
 ):
     yield
-    background_tasks.add_task(storage.save_state)
-    logger.info(f"Background task for save movie storage state added")
+    if request.method in UNSAFE_METHODS:
+        background_tasks.add_task(storage.save_state)
+        logger.info(f"Background task for save movie storage state added")
