e40fd1d1a62ada51a7449805274a017bfe07aa84
trifonovtema <artyom85@gmail.com>
2025-05-07 11:30:38 +0300

    specify responses for 401

--- movie-catalog/api/api_v1/movie_catalog/views/list_views.py
+++ movie-catalog/api/api_v1/movie_catalog/views/list_views.py
@@ -6,7 +6,7 @@ from starlette import status
 from api.api_v1.movie_catalog.crud import storage
 from api.api_v1.movie_catalog.dependencies import (
     save_movie_storage_state,
-    api_token_check,
+    api_token_check_for_unsafe_methods,
 )
 from schemas.movie import Movie, MovieCreate, MovieRead
 
@@ -14,9 +14,21 @@ router = APIRouter(
     prefix="/movies",
     tags=["Movies"],
     dependencies=[
+        Depends(api_token_check_for_unsafe_methods),
         Depends(save_movie_storage_state),
-        Depends(api_token_check),
     ],
+    responses={
+        status.HTTP_401_UNAUTHORIZED: {
+            "description": "Invalid token",
+            "content": {
+                "application/json": {
+                    "example": {
+                        "detail": "Unauthorized. Invalid token",
+                    },
+                },
+            },
+        },
+    },
 )
 
 
